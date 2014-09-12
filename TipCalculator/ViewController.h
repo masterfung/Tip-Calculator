d2b43a047885bce9914700a943c15fda5ebe2ba3
Tsung Hung <hungmasterj@gmail.com>
2014-09-11 18:59:28 -0700

    Initial Commit

---
+++ TipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalculator
+//
+//  Created by Tsung Hung on 9/11/14.
+//  Copyright (c) 2014 Tsung Hung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
